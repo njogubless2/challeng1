35e2ca2f1fef0d188176aa96fc8cbce881933a89
njogubless <pnjogubless@gmail.com>
2024-08-15 21:18:05 +0300

    adding the pages to the inetrface

---
+++ lib/pages/bible.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Bible extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Giving Screen Content'),
+      ),
+    );
+  }
+}
